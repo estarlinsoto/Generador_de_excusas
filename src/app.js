db7cbe0870391ae0a52bae08b1aa8c22805f7acf
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-08-14 22:36:00 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let whoLength = who.length;
+let whoR = Math.floor(Math.random() * whoLength);
+console.log(whoR);
+console.log(who[whoR]);
+
+let actionLength = action.length;
+let actionR = Math.floor(Math.random() * actionLength);
+console.log(actionR);
+console.log(action[actionR]);
+
+let whatLength = what.length;
+let whatR = Math.floor(Math.random() * whatLength);
+console.log(whatR);
+console.log(what[whatR]);
+
+let whenLength = when.length;
+let whenR = Math.floor(Math.random() * whenLength);
+console.log(whenR);
+console.log(when[whenR]);
+
+const result = `${who[whoR]}  ${action[actionR]}  ${what[whatR]}  ${when[whenR]}`;
+
+console.log(result);
+let excusa = document.getElementById("excuse");
+excusa.innerHTML(result);
