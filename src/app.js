c44996d9fa9e42997e651c94623716e015ec06a4
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-08-14 23:29:38 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,41 +1,41 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+window.onload = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+  let whoLength = who.length;
+  let whoR = Math.floor(Math.random() * whoLength);
 
-let whoLength = who.length;
-let whoR = Math.floor(Math.random() * whoLength);
-console.log(whoR);
-console.log(who[whoR]);
+  let actionLength = action.length;
+  let actionR = Math.floor(Math.random() * actionLength);
 
-let actionLength = action.length;
-let actionR = Math.floor(Math.random() * actionLength);
-console.log(actionR);
-console.log(action[actionR]);
+  let whatLength = what.length;
+  let whatR = Math.floor(Math.random() * whatLength);
 
-let whatLength = what.length;
-let whatR = Math.floor(Math.random() * whatLength);
-console.log(whatR);
-console.log(what[whatR]);
+  let whenLength = when.length;
+  let whenR = Math.floor(Math.random() * whenLength);
 
-let whenLength = when.length;
-let whenR = Math.floor(Math.random() * whenLength);
-console.log(whenR);
-console.log(when[whenR]);
+  const result = `${who[whoR]}  ${action[actionR]}  ${what[whatR]}  ${when[whenR]}`;
 
-const result = `${who[whoR]}  ${action[actionR]}  ${what[whatR]}  ${when[whenR]}`;
+  console.log(result);
+  let c = document.getElementById("excuse");
 
-console.log(result);
-let excusa = document.getElementById("excuse");
-excusa.innerHTML(result);
+  c.innerText = result;
+
+  let boton = document.getElementById("bExcusa");
+  boton.addEventListener("click", _ => {
+    location.reload();
+  });
+};
+//
